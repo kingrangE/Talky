a396c01aed214cc36e2f6bdd843d5931fb91915e
kingrangE <kilwon.jeon00@gmail.com>
2026-05-07 22:04:51 +0900

    fix: Neo4j 기본 비밀번호를 'neo4j' 가 아닌 값으로 변경

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
 
     NEO4J_URI: str = "bolt://localhost:7687"
     NEO4J_USER: str = "neo4j"
-    NEO4J_PASSWORD: str = "neo4j"
+    NEO4J_PASSWORD: str = "talky-dev-pw"
 
     APP_USER_ID: str = ""
     EVOLVE_BATCH: int = 5
